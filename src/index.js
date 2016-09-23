ce74743f39163f897ad912927af1e13fc95c04a5
Swiip <matthieu.lux@gmail.com>
2016-09-23 10:39:44 +0200

    load bg async, bg with mobile version

--- src/index.js
+++ src/index.js
@@ -1,54 +1,67 @@
 import './index.scss';
 
 function updateScales() {
-  var windowWidth = $(window).width();
+  const windowWidth = $(window).width();
 
-  var minLeftProps = {
+  const minLeftProps = {
     viewBox: [1700, 1000, 1180, 1750],
     x: 1650,
     y: 1020
-  }
+  };
 
-  var minRightProps = {
+  const minRightProps = {
     viewBox: [0, 1000, 1180, 1780],
     x: 0,
     y: 1000
-  }
+  };
 
-  var maxProps = {
+  const maxProps = {
     viewBox: [0, 0, 2831, 3761],
     x: 0,
     y: 0
-  }
+  };
 
-  var propsLeft = maxProps;
-  var propsRight = maxProps;
+  let propsLeft = maxProps;
+  let propsRight = maxProps;
 
   if (windowWidth <= 768) {
     propsLeft = minLeftProps;
     propsRight = minRightProps;
   }
 
-  $('svg.left, section .left svg').each(function () {
-    $(this).attr('viewBox', propsLeft.viewBox.join(' '));
+  const $this = $(this);
+
+  $('svg.left, section .left svg').each(() => {
+    $this.attr('viewBox', propsLeft.viewBox.join(' '));
   });
-  $('.left image').each(function () {
-    $(this).attr('x', propsLeft.x);
-    $(this).attr('y', propsLeft.y);
+  $('.left image').each(() => {
+    $this.attr('x', propsLeft.x);
+    $this.attr('y', propsLeft.y);
   });
 
-  $('svg.right, section .right svg').each(function () {
-    $(this).attr('viewBox', propsRight.viewBox.join(' '));
+  $('svg.right, section .right svg').each(() => {
+    $this.attr('viewBox', propsRight.viewBox.join(' '));
   });
-  $('.right image').each(function () {
-    $(this).attr('x', propsRight.x);
-    $(this).attr('y', propsRight.y);
+  $('.right image').each(() => {
+    $this.attr('x', propsRight.x);
+    $this.attr('y', propsRight.y);
   });
 }
 
-$(window).resize(function () {
+function waitForMainBackground() {
+  const div = document.querySelector('.palm-background');
+  const style = window.getComputedStyle(div, ':before');
+  const src = style['background-image'];
+  const url = src.match(/\((.*?)\)/)[1].replace(/('|")/g, '');
+  const img = new Image();
+  img.onload = () => document.body.classList.add('loaded');
+  img.src = url;
+}
+
+$(window).resize(() => {
   updateScales();
 });
-$(document).ready(function () {
+$(document).ready(() => {
   updateScales();
+  waitForMainBackground();
 });
