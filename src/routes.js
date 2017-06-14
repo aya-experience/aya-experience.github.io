78a8684b1860b3f9a336db1f1df8034aa6e321f5
FBerthelot <florent.berthelot35@gmail.com>
2017-06-14 21:00:44 +0200

    Use another method for analytic

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,18 @@ import React from "react"
 import { Route } from "react-router"
 import { PageContainer as PhenomicPageContainer } from "phenomic"
 
+import reactGa from "react-ga";
+
 import AppContainer from "./AppContainer"
 import Page from "./layouts/Page"
 import PageError from "./layouts/PageError"
 import Homepage from "./layouts/Homepage"
+
+const isBrowser = typeof window !== "undefined";
+
+if (isBrowser) {
+  reactGa.initialize("UA-74307110-4");
+}
 
 const PageContainer = (props) => (
   <PhenomicPageContainer
@@ -18,8 +26,16 @@ const PageContainer = (props) => (
   />
 )
 
+const logPageView = () => {
+  if (isBrowser) {
+    reactGa.set({ page: window.location.pathname });
+    reactGa.pageview(window.location.pathname);
+  }
+};
+
+
 export default (
   <Route component={ AppContainer }>
-    <Route path="*" component={ PageContainer } />
+    <Route path="*" component={ PageContainer } onEnter={logPageView} />
   </Route>
 )
