a9e33180cc9b20ca7b8d69e2dbc8d84b1a431ffb
Swiip <matthieu.lux@gmail.com>
2017-10-04 12:50:08 +0200

    add animated logo with css

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
 </template>
 
 <style>
-	@import url('https://fonts.googleapis.com/css?family=Lato:300,400,700');
+	/*@import url('https://fonts.googleapis.com/css?family=Lato:300,400,700');*/
 
 	* {
 		box-sizing: border-box;
