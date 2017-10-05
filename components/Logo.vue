af6d84e98a285ed52218a412598d680c2a3837cc
Swiip <matthieu.lux@gmail.com>
2017-10-05 16:03:20 +0200

    reverse kanji to use with a background

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,8 +1,13 @@
 <template>
-	<h1>
-		<svg xmlns="http://www.w3.org/2000/svg" version="1.2" viewBox="0 0 215 175" timelineBegin="onStart">
-			<title>AYA - &lt;creative experience&gt;</title>
-			<kanji/>
+	<main>
+		<nav v-show="loaded">
+			<a class="link vision" @mouseenter="enter('vision')" @mouseout="leave">Vision</a>
+			<a class="link real" @mouseenter="enter('real')" @mouseout="leave">Réalisations</a>
+			<a class="link method" @mouseenter="enter('method')" @mouseout="leave">Méthode</a>
+		</nav>
+		<div class="kanji-background" :class="kanjiClass"></div>
+		<kanji/>
+		<svg viewBox="0 0 215 175">
 			<g transform="matrix(.5 0 0 .5 53 82)">
 				<!-- /\ du premier A -->
 				<path class="line a-main" d="M 0,90 38,0 76,90"/>
@@ -23,23 +28,78 @@
 				</text>
 			</g>
 		</svg>
-	</h1>
+	</main>
 </template>
 
 <style scoped>
-h1 {
+.link {
+	color: white;
+	font-size: 3rem;
+	font-weight: 100;
+	display: inline-block;
 	position: fixed;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-	display: flex;
-	align-items: center;
-	justify-content: center;
+	cursor: pointer;
+}
+
+.link:hover {
+	transition: all .5s ease;
+	font-size: 4rem;
+}
+
+.link.vision {
+	top: 90vh;
+	left: 45vw;
+	width: 10vw;
+	text-align: center;
+}
+
+.link.real {
+	top: 50vh;
+	left: 5vw;
+}
+
+.link.method {
+	top: 50vh;
+	right: 5vw;
+}
+
+.kanji-background {
+	position: fixed;
+	top: 10.5vh;
+	left: calc(50vw - 39.5vh);
+	height: 79vh;
+	width: 79vh;
+	background-color: white;
+	animation-name: appear;
+	animation-duration: 5s;
+	animation-timing-function: linear;
+	background-size: cover;
+	background-position: center;
+	transition: all 3s ease;
+	opacity: .5;
+}
+
+.kanji-background.method {
+	background-image: url('/photos/rails.jpg');
+	opacity: 1;
+}
+
+.kanji-background.real {
+	background-image: url('/photos/mac.jpg');
+	opacity: 1;
+}
+
+.kanji-background.vision {
+	background-image: url('/photos/matrix.jpg');
+	opacity: 1;
 }
 
 svg {
+	position: fixed;
+	top: 10vh;
+	left: calc(50vw - 40vh);
 	height: 80vh;
+	width: 80vh;
 	fill: transparent;
 }
 
@@ -84,6 +144,12 @@ text {
 	stroke-dasharray: 60px;
 }
 
+@keyframes appear {
+	0% { opacity: 0; }
+	50% { opacity: 1; }
+	100% { opacity: 0.5; }
+}
+
 @keyframes a-main {
 	0% { stroke-dashoffset: 200px }
 	50% { stroke-dashoffset: 200px }
@@ -123,6 +189,23 @@ import Kanji from '~/components/Kanji.vue'
 export default {
 	components: {
 		Kanji
+	},
+	data () {
+		return {
+			loaded: false,
+			kanjiClass: 'white'
+		}
+	},
+	beforeMount () {
+		this.loaded = true
+	},
+	methods: {
+		enter (name) {
+			this.kanjiClass = name
+		},
+		leave () {
+			this.kanjiClass = 'white'
+		}
 	}
 }
 </script>
