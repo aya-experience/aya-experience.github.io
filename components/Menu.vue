5a34354c81183e4a9d1a00c855e69c7fa3c3d6f4
Swiip <matthieu.lux@gmail.com>
2018-01-23 15:48:17 +0100

    refacto splash with menu component outside of the logo

---
+++ components/Menu.vue
@@ -0,0 +1,68 @@
+<template>
+	<nav>
+		<a v-for="(link, index) of links" class="link"
+			:style="{ marginLeft: `${Math.sqrt(links.length - index) * 10}vh` }"
+			@mouseenter="enter(link)"
+			@mouseout="leave"
+			@click="go(link)">
+			{{ link.title }}
+		</a>
+	</nav>
+</template>
+
+<style scoped>
+nav {
+	position: fixed;
+	bottom: 20vh;
+	left: calc(50vw + 20vh);
+	z-index: 1;
+
+	display: flex;
+	flex-direction: column;
+	font-size: 20px;
+	font-weight: lighter;
+
+	transition: opacity .5s ease;
+	opacity: 0;
+}
+
+.loaded nav {
+	opacity: 1;
+}
+
+a {
+	color: #EBB815;
+	margin: 10px 5px;
+	cursor: pointer;
+	transition: color .5s ease;
+}
+
+a:hover {
+	color: white;
+}
+</style>
+
+<script>
+export default {
+	data () {
+		return {
+			links: [
+				{ title: 'Vision', path: '/vision', image: '/photos/rails.jpg' },
+				{ title: 'Réalisations', path: '/works', image: '/photos/mac.jpg' },
+				{ title: 'Méthode', path: '/method', image: '/photos/matrix.jpg' }
+			]
+		}
+	},
+	methods: {
+		enter (link) {
+			this.$emit('enter', link)
+		},
+		leave () {
+			this.$emit('leave')
+		},
+		go (link) {
+			this.$emit('go', link)
+		}
+	}
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,39 @@
 <template>
-	<section class="container">
-		<logo/>
+	<section class="container" :class="{ loading: !isLoaded, loaded: isLoaded }">
+		<aya-logo :hover="hover" :dive="dive" @loaded="loaded"/>
+		<aya-menu @enter="enter" @leave="leave" @go="go"/>
 	</section>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
+import Menu from '~/components/Menu.vue'
 
 export default {
+	data () {
+		return {
+			isLoaded: false,
+			hover: null,
+			dive: null
+		}
+	},
 	components: {
-		Logo
+		'aya-logo': Logo,
+		'aya-menu': Menu
+	},
+	methods: {
+		loaded () {
+			this.isLoaded = true
+		},
+		enter (link) {
+			this.hover = link
+		},
+		leave () {
+			this.hover = null
+		},
+		go (link) {
+			this.dive = link
+		}
 	}
 }
 </script>
-
-<style>
-</style>
