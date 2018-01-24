23d724043ebee8ff100908c1b1a0547c7885af0f
Swiip <matthieu.lux@gmail.com>
2018-01-24 17:30:37 +0100

    add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,9 +7,17 @@ module.exports = {
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: 'AYA Website & App' }
+			{ hid: 'description', name: 'description', content: 'AYA code & design the digital agency by Zenika' },
+			{ name: 'theme-color', content: '#ffffff' }
 		],
-		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{ rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+			{ rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+			{ rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+			{ rel: 'manifest', href: '/manifest.json' },
+			{ rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#000000' }
+		]
 	},
 	/*
 	** Customize the progress bar color
