4b6544d53c431aa6d494640cb5ec7de00c902df2
Swiip <matthieu.lux@gmail.com>
2018-01-29 17:41:48 +0100

    new css for the work menu

--- pages/work.vue
+++ pages/work.vue
@@ -1,80 +1,88 @@
 <template>
-	<div align="center" class="work-container">
+	<div class="work-container" :style="{ width: `${works.length * 20}vw` }">
 		<aya-back/>
 		<a
 			v-for="(work, index) in works"
 			class="work-preview"
-			v-bind:style="{
-				'background-image' : `${gradient(index)}, url(${work.illustrations[0].url})`
-			}"
+			@mouseenter="enter(index)"
+			@mouseleave="leave(index)"
+			:style="{ 'background-image' : background(work, index) }"
 		>
 			<div class="work-title">
-				<h1>{{work.client_name}}</h1>
-				<h2>{{work.project_name}}</h2>
+				<img :src="work.logo.url">
+				<!-- <h1>{{ work.client_name }}</h1> -->
+				<h2>{{ work.project_name }}</h2>
 			</div>
 		</a>
 	</div>
 </template>
 
 <style scoped>
-	div {
-		color: white;
-	}
+.work-container {
+	position: absolute;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	display: flex;
+	flex-direction: row;
+	color: white;
+}
 
-	.work-container{
-		overflow: hidden;
-		position: absolute;
-		display: flex;
-		top:0;
-		left:0;
-		right:0;
-		bottom:0;
-	}
+a.work-preview {
+	height: 100%;
+	width: 20vw;
+	transition: all .2s ease;
+	background-position: center;
+	background-size: auto 150%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
 
-	a.work-preview .work-title {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		height: 30%;
-		width: 50%;
-		margin: -15% 0 0 -25%;
-		text-transform: uppercase;
-		padding: 7px;
-		z-index: 1000;
-		color:#ffffff;
-		font-weight: 300;
-		font-size: 13px;
-		text-shadow: 0px 0px 8px black;
-	}
+a.work-preview:hover {
+	width: 30vw;
+	background-size: auto 100%;
+}
 
-	a.work-preview:hover .work-title {
-		-webkit-transition: all 200ms ease-in;
-		-webkit-transform: scale(1.5);
-		transition: all 200ms ease-in;
-		transform: scale(1.5);
-	}
+a.work-preview .work-title {
+	text-transform: uppercase;
+	padding: 7px;
+	z-index: 1000;
+	color: #ffffff;
+	font-weight: 300;
+	font-size: 13px;
+	text-shadow: 0px 0px 8px black;
+	transition: all .2s ease;
+	text-align: center;
+}
 
-	a.work-preview {
-		display: inline-block;
-		border: 0;
-		width: 20vw;
-		height: 100vh;
-		position: relative;
-		-webkit-transition: all 200ms ease-in;
-		-webkit-transform: scale(1);
-		transition: all 200ms ease-in;
-		transform: scale(1);
-		background-size: cover;
-	}
-	a.work-preview:hover {
-		flex-grow: 2;
-		box-shadow: 0px 0px 150px #000000;
-		z-index: 2;
-		-webkit-transition: all 200ms ease-in;
-		-webkit-transform: scale(1.5);
-		transition: all 200ms ease-in;
-		transform: scale(1.5);
-	}
+a.work-preview .work-title h1,
+a.work-preview .work-title h2 {
+	font-weight: normal;
+	transition: all .2s ease;
+}
+
+a.work-preview:hover .work-title h1 {
+	font-size: 50px;
+}
+
+a.work-preview:hover .work-title h2 {
+	font-size: 30px;
+}
+
+a.work-preview .work-title img {
+  display: block;
+  max-width:15vh;
+  max-height:15vh;
+  width: auto;
+  height: auto;
+	margin: auto;
+}
+
+a.work-preview:hover .work-title img {
+	max-width:30vh;
+  max-height:30vh;
+}
 </style>
 
 <script>
@@ -88,16 +96,51 @@ export default {
 	},
 	data () {
 		return {
-			works,
+			works: [...works, ...works], // dev hack to have more works
+			hoverIndex: null,
 			gradients: {
-				even: 'linear-gradient(#000000E0, #000000C0, #400040C0)',
-				odd: 'linear-gradient(#400040C0, #000000C0, #000000E0)'
+				even: 'linear-gradient(#000000C0, #000000C0)',
+				odd: 'linear-gradient(#000000E0, #000000E0)',
+				hover: [
+					'linear-gradient(150deg, #786CF0E0, #000000E0 60%)',
+					'linear-gradient(150deg, #C54E4EE0, #000000E0 60%)',
+					'linear-gradient(150deg, #5E9848F0, #000000E0 60%)'
+				]
 			}
 		}
 	},
+	created () {
+		if (process.browser) {
+			document.querySelector('html').addEventListener('mousewheel', this.handleWheel)
+		}
+	},
+	destroyed () {
+		if (process.browser) {
+			document.querySelector('html').removeEventListener('mousewheel', this.handleWheel)
+		}
+	},
 	methods: {
-		gradient (index) {
+		background (work, index) {
+			const image = `url(${work.illustrations[0].url})`
+			const gradient = this.hoverIndex === index
+				? this.activeGradient(index) : this.inactiveGradient(index)
+			return `${gradient}, ${image}`
+		},
+		inactiveGradient (index) {
 			return index % 2 === 0 ? this.gradients.even : this.gradients.odd
+		},
+		activeGradient (index) {
+			return this.gradients.hover[index % this.gradients.hover.length]
+		},
+		enter (index) {
+			this.hoverIndex = index
+		},
+		leave (index) {
+			this.hoverIndex = null
+		},
+		handleWheel (event, delta) {
+			document.querySelector('html').scrollLeft += event.deltaY
+			event.preventDefault()
 		}
 	}
 }
