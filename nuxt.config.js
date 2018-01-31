91ab3775d881d28846ec8507b3e74eec7b6e413d
FBerthelot <florent.berthelot35@gmail.com>
2018-01-31 17:23:41 +0100

    feat: improve accesibility

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ module.exports = {
 	*/
 	head: {
 		title: 'AYA code & design',
+		htmlAttrs: {
+			lang: 'fr'
+		},
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
