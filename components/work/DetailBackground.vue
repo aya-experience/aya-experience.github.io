19801419b62c48bde448a585aeb8f68cf453bba4
samy <yamane94@gmail.com>
2018-02-15 16:27:21 +0100

    feat: visualisation of works images
    wipe: background color change on detail work and responsive page
    
    wipe: Add bounce transition
    
    wipe: pointer cursor on arrows
    
    wipe: cancel button svg applied
    
    wipe: transition more smooth
    
    fix: next/prev button change to link
    
    fix: remove useless comment
    
    fix: rails.jpg update quality

--- components/work/DetailBackground.vue
+++ components/work/DetailBackground.vue
@@ -1,26 +1,45 @@
 <template>
-	<div class="bg-company">
-		<div class="bg-company-header">
-			<p>{{ projectName }}</p>
-			<button @click="hideWork">X</button>
+	<transition name="bounce">
+		<div class="bg-company" :style="{ backgroundColor: bgColor }">
+			<div class="bg-company-header">
+				<h1>{{ projectName }}</h1>
+				<img class="cancel" src="~/assets/icons/cancel.svg" @click="hideWork">
+			</div>
+			<img class="menu-bg" :src="menuBg">
 		</div>
-		<img :src="menuBg">
-	</div>
+	</transition>
 </template>
 
 <style scoped>
-.bg-company img {
-	display: block;
-	margin-left: auto;
-	margin-right: auto;
-	object-fit: cover;
+
+.bg-company {
+	position: absolute;
+	margin: 0 15vw 0 15vw;
+	transition: all .6s cubic-bezier(.55,0,.1,1);
 }
 
-.bg-company p {
+.bg-company h1 {
 	font-family: Lato;
 	font-weight: 900; 
-	font-size: 1.5rem;
-	line-height: 2rem;
+	font-size: 2rem;
+	margin-left: 2vw;
+}
+
+.cancel {
+	margin-right: 0;
+	margin-top: 2vh;
+	margin-right: 2vh;
+	margin-bottom: 2vh;
+	cursor: pointer;
+	width: auto;
+	height: auto;
+	max-height: 3vh;
+	max-width: 10vw;
+}
+
+.menu-bg {
+	width: 100%;
+	border: solid 5px #727272;
 }
 
 .bg-company div {
@@ -29,7 +48,35 @@
 
 .bg-company-header {
 	display: flex;
-	justify-content: space-around;
+	justify-content: space-between;
+	align-items: center;
+	margin-top: 10vh;
+}
+
+@media all and (max-width: 1024px) {
+	.bg-company { margin-left: 8vw; margin-right: 8vw;}
+
+}
+
+/* animation */
+.bounce-enter-active {
+	animation: bounce-in .5s;
+}
+
+.bounce-leave-active {
+	animation: bounce-in .8s reverse;
+}
+
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
 
@@ -41,6 +88,10 @@ export default {
 			required: true
 		},
 		projectName: {
+			type: String,
+			required: true
+		},
+		bgColor: {
 			type: String,
 			required: true
 		}
