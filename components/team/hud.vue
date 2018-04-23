c0ff282c79f5f5e526b56d61ebbe7b5331a2a9e4
samy <yamane94@gmail.com>
2018-04-23 17:03:17 +0200

    feat: vr components

---
+++ components/team/hud.vue
@@ -0,0 +1,38 @@
+<template>
+	<a-entity position="0 0 0.05" :rotation="rotation">
+		<a-image
+			id="hudLine"
+			scale="0.1 0.3 0.1"
+			position="0.18 0.4 0"
+			src="#hud"/>
+		<a-image
+			position="0.3 0.5 0"
+			scale="0.1 0.1 0.1"
+			:src="member.icon"/>
+		<a-text position="0.25 0.38 0" scale="0.12 0.12 0.12" :value="member.name"/>
+		<a-text position="0.25 0.32 0" scale="0.12 0.12 0.12" :value="member.role"/>
+		<a-text position="0.25 0.26 0" scale="0.12 0.12 0.12" :value="member.desc"/>
+	</a-entity>
+</template>
+
+<script>
+export default {
+	props: {
+		position: {
+			type: String,
+			default: '1 0 0'
+		},
+		member: {
+			type: Object,
+			default: () => {}
+		}
+	},
+	computed: {
+		rotation() {
+			const camera = document.querySelector('#camera').getAttribute('rotation');
+			return camera.x + ' ' + (camera.y + 45) + ' ' + camera.z;
+		}
+	}
+};
+</script>
+
---
+++ components/team/world.vue
@@ -0,0 +1,33 @@
+<!--eslint-disable-->
+<template>
+	<a-entity id="world">
+		<a-entity :position="position" scale="0.002 0.002 0.002" :gltf-model="type"/>
+		<a-entity @click="changeMap(true)" position="-0.536 1.007 0.437" rotation="0 45 0" scale="0.5 0.5 0.5" obj-model="obj: #arrow-obj; mtl: #arrow-mtl"/>
+		<a-entity @click="changeMap(false)" position="-0.40 1.007 0.13" rotation="0 -110 0" scale="0.5 0.5 0.5" obj-model="obj: #arrow-obj; mtl: #arrow-mtl"/>
+		<slot/>
+	</a-entity>
+</template>
+
+<script>
+export default {
+	props: {
+		type: {
+			type: String,
+			default: '/vr/island/scene.gltf'
+		}
+	},
+	data() {
+		return {
+			position: '4.738 0.768 0.000',
+			posX: 4.738
+		};
+	},
+	methods: {
+		changeMap(back) {
+			this.posX = back ? this.posX - 10.938 : this.posX + 10.938;
+			this.position = this.posX + ' 0.768 0.000';
+		}
+	}
+};
+</script>
+
